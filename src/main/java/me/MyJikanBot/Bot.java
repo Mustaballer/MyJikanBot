636588d382e50c8fd6d52718a1d31ab20859a7c9
Abdulrahman Mohammed <abdulrahman@Abdulrahmans-MacBook-Pro.local>
2020-02-22 16:47:03 -0500

    implemented Gradle and RapidAPI

--- src/main/java/me/MyJikanBot/Bot.java
+++ src/main/java/me/MyJikanBot/Bot.java
@@ -12,6 +12,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 
 public class Bot {
@@ -20,7 +25,7 @@ public class Bot {
 	
 	// Main Method
 	public static void main(String[] args) throws LoginException {
-		jda = new JDABuilder(AccountType.BOT).setToken("Njc2NjAxMzQwOTk4NDUxMjIw.XkSJ9Q.f6pMhlyYx3zXBmRn-PIdRzklJl8").build();
+		jda = new JDABuilder(AccountType.BOT).setToken("Njc2NjAxMzQwOTk4NDUxMjIw.XlGffw.KJG7EBss4K_AN2W5V-4X9egV76s").build();
 		jda.getPresence().setStatus(OnlineStatus.IDLE);
 		jda.getPresence().setActivity(Activity.watching("20th century boys"));
 
@@ -30,5 +35,22 @@ public class Bot {
 		jda.addEventListener(new GuildMemberLeave());
 		jda.addEventListener(new GuildMessageReceived());
 		jda.addEventListener(new GuildMessageReactionAdd());
+
+		OkHttpClient client = new OkHttpClient();
+
+		Request request = new Request.Builder()
+				.url("https://jikan1.p.rapidapi.com/meta/requests/anime/today")
+				.get()
+				.addHeader("x-rapidapi-host", "jikan1.p.rapidapi.com")
+				.addHeader("x-rapidapi-key", "96a73e158bmshe52e62c5001ba3fp197597jsnaf4ed14bff71")
+				.build();
+
+		try {
+			Response response = client.newCall(request).execute();
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
